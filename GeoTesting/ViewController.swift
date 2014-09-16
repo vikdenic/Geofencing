4cc367668568d7080f8ff21e6d68e9d0b2dc3653
vikdenic <vik.denic@gmail.com>
2014-09-16 11:52:40 -0500

    first commit

--- GeoTesting/ViewController.swift
+++ GeoTesting/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    let locationManager = CLLocationManager()
+    var location = CLLocation()
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestAlwaysAuthorization()
+        mapView.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func updateLocation(sender: UIBarButtonItem) {
+        let spanX = 0.58
+        let spanY = 0.58
+        location = locationManager.location
+        println("Received location:\(location.description)")
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(locationManager.location.coordinate.latitude, locationManager.location.coordinate.longitude), span: MKCoordinateSpanMake(spanX, spanY))
+        mapView.setRegion(region, animated: true)
     }
 
+    @IBAction func monitorThisRegion(sender: UIBarButtonItem) {
+        let center = CLLocationCoordinate2D(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
 
+        locationManager.startMonitoringForRegion(CLCircularRegion(circularRegionWithCenter: center, radius: 20.0, identifier: "MMChicago"))
+    }
+
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        println("welcome")
+    }
+
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        println("bye")
+    }
+
+    func locationManager(manager: CLLocationManager!, didStartMonitoringForRegion region: CLRegion!) {
+        println("monitoring...")
+    }
 }
 
