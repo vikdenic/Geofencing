3ff9a691a37043d40f4150443e9cbee9b4940c79
vikdenic <vik.denic@gmail.com>
2014-09-16 09:43:55 -0500

    Initial Commit

---
+++ GeoTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoTesting
+//
+//  Created by Johnny Appleseed on 9/16/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
